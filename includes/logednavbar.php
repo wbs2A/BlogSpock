291189374c715e9f3a0198bcc8aea6c409cf7450
Wesley Barbosa <34480299+wbs2A@users.noreply.github.com>
2018-09-24 15:14:36 -0400

    Alterações no user menu e divs relacionadas aos posts

--- includes/logednavbar.php
+++ includes/logednavbar.php
@@ -20,3 +20,49 @@
 
     </ul>
 </nav>
+
+
+DELIMITER $$
+USE `blogspock`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `id3097493_blogspock`.`creation_time` AFTER INSERT ON `post` FOR EACH ROW
+BEGIN
+update post set post.horaCriacao=now() where post.idpost=last_insert_id();
+END$$
+
+USE `blogspock`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `blogspock`.`comentario_AFTER_INSERT` AFTER INSERT ON `comentario` FOR EACH ROW
+BEGIN
+update comentario set comentario.horaCriacao=now() where comentario.idcomentario=last_insert_id();
+
+END$$
+
+use blogspock $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `del_post`(`id` INT)
+BEGIN
+SET SESSION AUTOCOMMIT=0;
+SET AUTOCOMMIT =0;
+START TRANSACTION;
+DELETE FROM comentario  WHERE comentario.post_idpost = id;
+DELETE FROM post WHERE post.idpost = id;
+SELECT "suscesso" as suscesso;
+COMMIT;
+SET SESSION AUTOCOMMIT=1;
+SET AUTOCOMMIT =1;
+END $$
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `del_user`(`id` INT)
+BEGIN
+SET SESSION AUTOCOMMIT = 0;
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+DELETE FROM comentario WHERE comentario.usuario_idusuario = id;
+DELETE FROM post WHERE post.usuario_idusuario = id;
+DELETE FROM solicitacoes
+DELETE FROM usuario WHERE usuario.idusuario = id;
+SELECT "suscesso" as suscesso;
+COMMIT;
+SET SESSION AUTOCOMMIT=1;
+SET AUTOCOMMIT =1;
+END $$
+DELIMITER ;
--- includes/menus/user.php
+++ includes/menus/user.php
@@ -1,3 +1,2 @@
         <li id="perfil" style="margin-left: 72%"><a href="#perfil"> Perfil </a> </li>
         <li><a href="#">Solicitação </a></li>
-        <li><a href="#">Reclamações</a></li>
